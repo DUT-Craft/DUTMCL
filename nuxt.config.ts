b4daff5575ff8606d4203637d2c4767f51e2a7d0
FOXBALL-ONE <admin@foxball.dev>
2025-10-23 18:04:02 +0800

    initialize project structure with Tauri and Nuxt setup

---
+++ nuxt.config.ts
@@ -0,0 +1,52 @@
+import AutoImport from 'unplugin-auto-import/vite'
+import {NaiveUiResolver} from 'unplugin-vue-components/resolvers'
+import Components from 'unplugin-vue-components/vite'
+
+// https://nuxt.com/docs/api/configuration/nuxt-config
+export default defineNuxtConfig({
+  compatibilityDate: "2024-11-01",
+  ssr: false,
+  devtools: {
+    enabled: true,
+    timeline: {
+      enabled: true,
+    },
+  },
+  devServer: {host:'localhost'},
+  modules: [
+    'nuxtjs-naive-ui'
+  ],
+  vite: {
+    clearScreen: false,
+    envPrefix: ['VITE_', 'TAURI_'],
+    server: {
+      // Tauri需要一个确定的端口
+      strictPort: true,
+    },
+    plugins: [
+      AutoImport({
+        imports: [
+          {
+            'naive-ui': [
+              'useDialog',
+              'useMessage',
+              'useNotification',
+              'useLoadingBar'
+            ]
+          }
+        ]
+      }),
+      Components({
+        resolvers: [NaiveUiResolver()]
+      })
+    ]
+  },
+  build: {
+    transpile: ['naive-ui', "vueuc"]
+  },
+  nitro: {
+    experimental: {
+      wasm: true
+    },
+  }
+})
