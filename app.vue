b470cb903b5b71a6c48302592d6d94163b17c06d
FOXBALL-ONE <admin@foxball.dev>
2025-10-24 10:29:14 +0800

    feat(ui): 集成消息提示组件
    - 在应用根组件中添加 NMessageProvider 组件
    - 导入并注册 Naive UI 的消息提示组件- 为页面布局包裹消息提示上下文提供者- 支持全局使用 $message 方法显示提示信息

--- app.vue
+++ app.vue
@@ -1,13 +1,15 @@
 <template>
   <n-config-provider>
-    <nuxt-layout>
-      <nuxt-page style="min-height: calc(100dvh - 61px)"/>
-    </nuxt-layout>
+    <n-message-provider>
+      <nuxt-layout>
+        <nuxt-page style="min-height: calc(100dvh - 61px)"/>
+      </nuxt-layout>
+    </n-message-provider>
   </n-config-provider>
 </template>
 
 <script lang="ts" setup>
 // 引入 Naive UI 组件库的配置提供者组件，用于全局配置 Naive UI 组件
-import {NConfigProvider} from 'naive-ui';
+import {NConfigProvider, NMessageProvider} from 'naive-ui';
 // 全局样式导入
 </script>
